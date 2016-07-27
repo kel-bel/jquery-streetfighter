fbdb2496093350b61746950efd4c85a7e616336f
Kelly Ceriani <Kelly Ceriani>
2016-07-27 14:58:35 -0400

    js/

--- js/app.js
+++ js/app.js
@@ -87,12 +87,13 @@ $(document).ready(function() {
 			$('.hulk-cool').hide();x
 		}
 	});
-});
 
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
-}
+};
+
+});
 
 
