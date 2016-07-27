daf5414fc5591f7cc841e17a696ecad3382ac810
Kelly Ceriani <Kelly Ceriani>
2016-07-27 14:11:37 -0400

    js/

---
+++ js/app.js
@@ -0,0 +1,105 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide()
+		$('.ryu-ready').show()
+	})
+	.mouseleave(function(){
+		$('.ryu-still').show()
+		$('.ryu-ready').hide()
+	})
+	.mousedown(function(){
+		playHadouken();
+		$('.ryu-ready').hide()
+		$('.ryu-throwing').show()
+		$('.hadouken').show().animate(
+			{'left': '1200px'},
+			550,
+			function() {
+				$(this).hide();
+				$(this).css('left', '625px')
+			}
+		)
+		$('.hadouken').finish().show()
+			.animate(
+				{'left': '1200px'},
+				550,
+				function() {
+					$(this).hide();
+					$(this).css('left', '625px');
+				}
+		)		
+	})
+	.mouseup(function(){
+		$('.ryu-throwing').hide()
+		$('.ryu-ready').show()
+	})
+	$(document).keydown(function(e) {
+		if (e.keyCode == 88) { 
+			$('.ryu-still').hide()
+			$('.ryu-cool').show()
+		}
+	})
+	.keyup(function(e) {
+		if (e.keyCode == 88) {
+			$('.ryu-still').show()
+			$('.ryu-cool').hide()
+		}
+	});
+});
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
+
+$(document).ready(function() {
+	$('.hulk-ryu').mouseenter(function() {
+		$('.hulk-still').hide()
+		$('.hulk-ready').show()
+	})
+	.mouseleave(function(){
+		$('.hulk-still').show()
+		$('.hulk-ready').hide()
+	})
+	.mousedown(function(){
+		playHadouken();
+		$('.hulk-ready').hide()
+		$('.hulk-throwing').show()
+		$('.hulk-hadouken').show().animate(
+			{'left': '500px'},
+			550,
+			function() {
+				$(this).hide();
+				$(this).css('left', '1200px')
+			}
+		)
+		$('.hulk-hadouken').finish().show()
+			.animate(
+				{'left': '500px'},
+				550,
+				function() {
+					$(this).hide();
+					$(this).css('left', '1200px');
+				}
+		)		
+	})
+	.mouseup(function(){
+		$('.hulk-throwing').hide()
+		$('.hulk-ready').show()
+	})
+		$(document).keydown(function(e) {
+		if (e.keyCode == 88) { 
+			$('.hulk-still').hide()
+			$('.hulk-cool').show()
+		}
+	})
+	.keyup(function(e) {
+		if (e.keyCode == 88) {
+			$('.hulk-still').show()
+			$('.hulk-cool').hide()
+		}
+	});
+});
+
+	
